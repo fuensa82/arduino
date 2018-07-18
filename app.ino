fa6f0176533b080063d64eaa08e3d422764429a2
Victor Palomo <vPalomo@fuensalida.com>
2018-07-18 12:11:23 +0200

    Arduino funciones toldo basicas completas

--- app.ino
+++ app.ino
@@ -1,20 +1,21 @@
-const int Led13=13;
+
+const int led13=13;
+
 void setup()
 {
 	Serial.begin(9600);
-    pinMode(Led13,OUTPUT);
+    pinMode(led13,OUTPUT);
 }
 
-void loop()
-{
+void loop(){
 	if(Serial.available()>0){
         String data=Serial.readStringUntil('\n');
         Serial.print("Datos desde VSC: ");
         Serial.println(data);
         if(data=="on"){
-            digitalWrite(Led13,HIGH);
+            digitalWrite(led13,HIGH);
         }else if(data=="off"){
-            digitalWrite(Led13,LOW);
+            digitalWrite(led13,LOW);
         }
     }
 }
--- toldo.ino
+++ toldo.ino
@@ -1,4 +1,4 @@
-const int Led13 = 13;
+const int led13 = 13;
 const int bajaToldo=9;
 const int paraToldo=11;
 const int subeToldo=10;
@@ -6,7 +6,7 @@ const int subeToldo=10;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(Led13, OUTPUT);
+  pinMode(led13, OUTPUT);
   pinMode(bajaToldo,OUTPUT);
   pinMode(paraToldo,OUTPUT);
   pinMode(subeToldo,OUTPUT);
@@ -19,20 +19,41 @@ void loop()
     String data = Serial.readStringUntil('\n');
     Serial.print("Datos Leidos: ");
     Serial.println(data);
-    digitalWrite(Led13,HIGH);
-    if(data=="sube"){
+    //digitalWrite(led13,HIGH);
+    if(data=="quitaToldo"){
       digitalWrite(subeToldo,HIGH);
+      Serial.println("quitaToldo");
+      digitalWrite(led13,LOW);
       delay(50);
       digitalWrite(subeToldo,LOW);
-    }else if(data=="para"){
+    }else if(data=="paraToldo"){
       digitalWrite(paraToldo,HIGH);
+      Serial.println("paraToldo");
       delay(50);
       digitalWrite(paraToldo,LOW);
-    }else if(data=="baja"){
+      delay(200);
+      digitalWrite(led13,LOW);
+      delay(200);
+      digitalWrite(led13,HIGH);
+      delay(200);      
+      digitalWrite(led13,LOW);
+      delay(200);
+      digitalWrite(led13,HIGH);
+      delay(200);
+      digitalWrite(led13,LOW);
+      delay(200);
+      digitalWrite(led13,HIGH);
+      delay(200);
+      digitalWrite(led13,LOW);
+      
+      
+    }else if(data=="ponToldo"){
       digitalWrite(bajaToldo,HIGH);
+      Serial.println("Bajando");
+      digitalWrite(led13,HIGH);
       delay(50);
       digitalWrite(bajaToldo,LOW);
     }
-    digitalWrite(Led13,LOW);
+    //digitalWrite(Led13,LOW);
   }
 }
